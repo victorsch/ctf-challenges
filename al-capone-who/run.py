cbe497a03d85f23212ca641484b88f54b5e66959
victor.schiavo <victors@yellowbasket.io>
2023-03-21 01:20:55 -0400

    fix

--- al-capone-who/run.py
+++ al-capone-who/run.py
@@ -53,7 +53,7 @@ while True:
                 client_socket.send(str(f"Your balance is: {balance}\n").encode('utf-8'))
             
         except BrokenPipeError as e:
-            continue:
+            continue
         except Exception as e:
             client_socket.send(str(e).encode('utf-8'))
             # close the connection
