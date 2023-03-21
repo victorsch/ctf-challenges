fbea12d28d1b7db59558ea42efe710c239555b76
victor.schiavo <victors@yellowbasket.io>
2023-03-21 01:30:13 -0400

    Fix run issue

--- al-capone-who/run.py
+++ al-capone-who/run.py
@@ -17,53 +17,52 @@ server_socket.listen()
 while True:
     # accept a connection
     client_socket, addr = server_socket.accept()
-    running = True
     balance = 1543.27
-    while running:
-        print(f'Connection from {addr} has been established!')
-        
-        client_socket.send(str("\n").encode('utf-8'))
-        client_socket.send(str("Welcome to your banking application! Please tell us what you would like to do: \n").encode('utf-8'))
-        client_socket.send(str("1. Deposit\n").encode('utf-8'))
-        client_socket.send(str("2. Withdraw\n").encode('utf-8'))
-        client_socket.send(str("3. Balance\n").encode('utf-8'))
+    
+    print(f'Connection from {addr} has been established!')
+    
+    client_socket.send(str("\n").encode('utf-8'))
+    client_socket.send(str("Welcome to your banking application! Please tell us what you would like to do: \n").encode('utf-8'))
+    client_socket.send(str("1. Deposit\n").encode('utf-8'))
+    client_socket.send(str("2. Withdraw\n").encode('utf-8'))
+    client_socket.send(str("3. Balance\n").encode('utf-8'))
 
-        # receive the data from the client
-        data = client_socket.recv(1024)
+    # receive the data from the client
+    data = client_socket.recv(1024)
 
-        # decode the data into a string
-        data_str = data.decode('utf-8')
+    # decode the data into a string
+    data_str = data.decode('utf-8')
+    
+    try:
+        if (str(data_str.strip()) == "1"):
+            client_socket.send(str(f"How much would you like to deposit?\n").encode('utf-8'))
+            deposit_amount = client_socket.recv(1024).decode('utf-8')
+            client_socket.send(str(f"You have deposited {deposit_amount}\n").encode('utf-8'))
+            result = eval(str(f"{deposit_amount.strip()} + {balance}"))
+            balance = result
+            client_socket.send(str(f"Your new balance is: {result}\n").encode('utf-8'))
+        elif (str(data_str.strip()) == "2"):
+            client_socket.send(str("How much would you like to withdraw?\n").encode('utf-8'))
+            amount = client_socket.recv(1024).decode('utf-8')
+            client_socket.send(str(f"You have withdrawn {amount}\n").encode('utf-8'))
+            result = eval(str(f"{balance} - {amount}"))
+            balance = result
+            client_socket.send(str(f"Your new balance is: {result}\n").encode('utf-8'))
+        elif (str(data_str.strip()) == "3"):
+            client_socket.send(str(f"Your balance is: {balance}\n").encode('utf-8'))
         
-        try:
-            if (str(data_str.strip()) == "1"):
-                client_socket.send(str(f"How much would you like to deposit?\n").encode('utf-8'))
-                deposit_amount = client_socket.recv(1024).decode('utf-8')
-                client_socket.send(str(f"You have deposited {deposit_amount}\n").encode('utf-8'))
-                result = eval(str(f"{deposit_amount.strip()} + {balance}"))
-                balance = result
-                client_socket.send(str(f"Your new balance is: {result}\n").encode('utf-8'))
-            elif (str(data_str.strip()) == "2"):
-                client_socket.send(str("How much would you like to withdraw?\n").encode('utf-8'))
-                amount = client_socket.recv(1024).decode('utf-8')
-                client_socket.send(str(f"You have withdrawn {amount}\n").encode('utf-8'))
-                result = eval(str(f"{balance} - {amount}"))
-                balance = result
-                client_socket.send(str(f"Your new balance is: {result}\n").encode('utf-8'))
-            elif (str(data_str.strip()) == "3"):
-                client_socket.send(str(f"Your balance is: {balance}\n").encode('utf-8'))
-            
-        except BrokenPipeError as e:
-            running = False
-            client_socket.close()
-            break
-        except KeyboardInterrupt:
-            print("\nKeyboardInterrupt detected. Closing server...")
-            # close the server socket
-            running = False
-            client_socket.close()
-            break
-        except Exception as e:
-            client_socket.send(str(e).encode('utf-8'))
-            # close the connection
-            client_socket.close()
-            running = False
+    except BrokenPipeError:
+        running = False
+        client_socket.close()
+        break
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected. Closing server...")
+        # close the server socket
+        running = False
+        client_socket.close()
+        break
+    except Exception as e:
+        client_socket.send(str(e).encode('utf-8'))
+        # close the connection
+        client_socket.close()
+        running = False
